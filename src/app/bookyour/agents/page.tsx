bff572f0f28e2af7c039e6a73afb6bc2983a018b
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2024-12-06 22:48:28 +0530

    second page complete

--- src/app/bookyour/agents/page.tsx
+++ src/app/bookyour/agents/page.tsx
@@ -1,3 +1,9 @@
+import Image from 'next/image';
+import Contact from '../../../../public/contact.png';
+import Agent from '../../../../public/boycontacts.png';
+import John from '../../../../public/young-man.png';
+import Mirra from '../../../../public/woman.png';
+import Link from 'next/link'
 
 export default function Agents() {
     return (
@@ -5,10 +11,60 @@ export default function Agents() {
             <div className="flex items-center justify-center h-screen">
 
                 <div className="justify-center items-center border border-slate-800 p-4 w-1/2 h-auto divide-y divide-dashed hover:divide-solid">
-                    <div className="flex justify-center items-center m-4 mb-6 text-4xl">Available Service</div>
+            
+                    <div className="flex w-full h-full justify-between">
+                      
+                        <div className="w-1/2">
+                        <div className='h-full justify-center items-center flex-col flex space-y-8'>
+                            <div className="text-lg mt-4 w-3/4">
+                            <div  className="justify-center items-center flex ">
+                                <Image src={Contact} alt="Contact Icon"/>
+                                </div>
+                                <div className="font-bold flex justify-center items-center">Agents</div>
+                                <span className="text-gray-600 w-1/6">
+                                    Please select an Agent that will be providing you a service
+                                </span>
+                            </div>
+                            <div className="flex mt-4 items-center">
+                                Contact us
+                                <span className="text-blue-500 ml-2">+91 999xxxxx</span>
+                            </div>
+                            </div>
+                        </div>
 
+                    
+                        <div className="w-1/2">
+                        <div className="flex justify-center items-center m-4 mb-6 text-4xl">Available Agents</div>
+                            <ul className="space-x-4 flex">
+                                
+                                <li className=" space-x-4 outline outline-2 outline-slate-500 hover:outline-blue-500 p-4 rounded-lg w-2/3">
+                                   <Image src={Agent} alt="Any Agent" className="w-14 h-14 flex justify-center items-center m-3"  /> 
+                                  
+                                        <div className="font-semibold">Any Agent</div>
+                                    
+                                </li>
+
+                                
+                                <li className="items-center space-x-4 outline outline-2 outline-slate-500 hover:outline-blue-500 p-4 rounded-lg w-2/3">
+                                    <div><Image src={John} alt="John Icon" className="w-14 h-14 m-3" /></div>
+                                    <div>
+                                        <div className="font-semibold">John</div>
+                                    </div>
+                                </li>
+
+                              
+                                <li className=" items-center space-x-4 outline outline-2 outline-slate-500 hover:outline-blue-500 p-4 rounded-lg w-2/3">
+                                    <Image src={Mirra} alt="Hair Wash Icon" className="w-14 h-14 m-3" />
+                                    <div>
+                                        <div className="font-semibold font-">Mirra</div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <div className='mt-6'><Link href="/bookyour" className='font-bold text-gray-500'>🔙Back</Link></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
--- src/app/bookyour/page.tsx
+++ src/app/bookyour/page.tsx
@@ -3,49 +3,46 @@ import Blowdry from '../../../public/blow-dry.png';
 import Scissors from '../../../public/scissors.png';
 import Tube from '../../../public/tube.png';
 import Link from 'next/link'
-import Agents from './components/agents';
+// import Agents from './components/agents';
 export default function BookApp() {
     return (
         <div className="flex items-center justify-center h-screen">
-       
-            <div className="justify-center items-center border border-slate-800 p-4 w-1/2 h-auto divide-y divide-dashed hover:divide-solid">
-               
-                <div className="flex justify-center items-center m-4 mb-6 text-4xl">Available Service</div>
 
-              
+            <div className="justify-center items-center border border-slate-800 p-4 w-1/2 h-auto divide-y divide-dashed hover:divide-solid">
+
                 <div className="flex w-full flex-nowrap h-full justify-between">
-                  
+
                     <div className="w-1/2">
-                        <div className="space-y-8">
+                        <div className="space-y-8 flex flex-col justify-center items-center h-full">
                             <div className="text-lg mt-4 w-3/4">
-                                <div className="font-bold justify-center items-center flex">Service Selection</div>
+                                <div className="font-bold flex">Service Selection</div>
                                 <span className="text-gray-600 w-1/6">Please select a service for which you want to schedule an appointment</span>
                             </div>
-                            <div className="flex mt-4 items-center">
-                                Contact us
-                                <span className="text-blue-500 ml-2">+91 999xxxxx</span>
+                            <div className='w-3/4'>
+                                ContactUs:-<span className="text-blue-500">+91 999xxxxx</span>
                             </div>
                         </div>
                     </div>
 
-                    
+
                     <div className="w-1/2">
-                        <ul className="space-y-6">
-                           
-                            <li 
-                                className="flex items-center space-x-4 outline outline-2 outline-offset-2 hover:outline-blue-500 w-auto  p-2">
-                               
-                                <Image src={Blowdry} alt="Blowdry Icon" className="w-10 h-10" /> 
+                        <div className="flex justify-center items-center m-4 mb-6 text-4xl">Available Service</div>
+                        <ul className="space-y-5">
+
+                            <Link href="/bookyour/agents"><li
+                                className="flex items-center space-x-4 outline outline-2 outline-offset-2  outline-slate-500 hover:outline-blue-500 w-auto  p-2">
+
+                                <Image src={Blowdry} alt="Blowdry Icon" className="w-10 h-10" />
                                 <div>
                                     <div>Hair Coloring</div>
                                     <div className="text-sm text-gray-600">Change Color of hair permanently</div>
                                 </div>
-                            </li>
+                            </li></Link>
 
-                            
-                            <Link href="/bookyour/agents"><li 
-                                className="flex items-center space-x-4 outline outline-2 outline-offset-2  hover:outline-blue-500 w-auto p-2">
-                                
+
+                            <Link href="/bookyour/agents"><li
+                                className="flex mt-9 items-center space-x-4 outline outline-2 outline-offset-2 outline-slate-500 hover:outline-blue-500 w-auto p-2">
+
                                 <Image src={Scissors} alt="Scissors Icon" className="w-10 h-10" />
                                 <div>
                                     <div>Hair Cut</div>
@@ -53,19 +50,19 @@ export default function BookApp() {
                                 </div>
                             </li></Link>
 
-                            
-                            <li 
-                                className="flex items-center space-x-4 outline outline-2 outline-offset-2  hover:outline-blue-500 w-auto  p-2">
-                               
-                                <Image src={Tube} alt="Tube Icon" className="w-10 h-10" /> 
+
+                            <Link href="/bookyour/agents"><li
+                                className="flex mt-9  items-center space-x-4 outline outline-2 outline-offset-2 outline-slate-500  hover:outline-blue-500 w-auto  p-2">
+
+                                <Image src={Tube} alt="Tube Icon" className="w-10 h-10" />
                                 <div>
                                     <div>Hair Wash</div>
                                     <div className="text-sm text-gray-600">Come experience a perfect haircut today with the best stylists</div>
                                 </div>
-                            </li>
+                            </li></Link>
                         </ul>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
